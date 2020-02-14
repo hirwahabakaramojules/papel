7e281eea1f3b802161d72f996ad7d689b614d76e
hirwahabakaramojules <hirwahabakaramojules16@yahoo.com>
2020-02-14 08:40:58 +0200

    Updates

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -1,6 +1,7 @@
 const bText = document.getElementById("b-text");
 const select = document.getElementById("select");
-
+const deleteBtn = document.getElementById("delete-btn");
+const infoDiv = document.getElementById("info-container");
 
 select.addEventListener("change", ()=>{
     console.log(select.value);
@@ -8,4 +9,9 @@ select.addEventListener("change", ()=>{
 select.addEventListener("click", ()=>{
     console.log(select.value);
     bText.innerText = select.value;
+});
+
+deleteBtn.addEventListener("click",()=>{
+    console.log("im clicked");
+    infoDiv.style.display = "none";
 })
