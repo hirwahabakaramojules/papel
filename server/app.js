763c21017f0fcd26ac0a2c28f4087db1edef0181
hirwahabakaramojules <hirwahabakaramojules16@yahoo.com>
2020-02-18 04:06:52 -0800

    chore added folder structure and configured babel and eslint and intalled express

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+// const babel = require("@babel/core").transform("code", options);
+const app = express();
+// app.use(json());
+
+
+
+app.listen(3000, () => console.log("listening on port 3000!!!!"));
